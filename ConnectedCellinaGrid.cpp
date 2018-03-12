fbcb8352a7b7e0a796246ce05e5d5e289c4d942d
Shruti Nair <nairshru.95@gmail.com>
2018-03-11 23:16:11 -0700

    Update ConnectedCellinaGrid.cpp

--- ConnectedCellinaGrid.cpp
+++ ConnectedCellinaGrid.cpp
@@ -1,72 +1,41 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
-
+#include<bits/stdc++.h>
+#define M 4
+#define N 4
 using namespace std;
 
-int calc(vector< vector<int> > &grid,int i,int j){
-    int c;
-    if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size())
-        return 0;
-    if(grid[i][j]==0)
-        return 0;
-    c=grid[i][j]--;
-    c+=calc(grid,i-1,j);
-    c+=calc(grid,i+1,j);
-    c+=calc(grid,i,j-1);
-    c+=calc(grid,i,j+1);
-    c+=calc(grid,i-1,j-1);
-    c+=calc(grid,i-1,j+1);
-    c+=calc(grid,i+1,j-1);
-    c+=calc(grid,i+1,j+1);
-    return c;
-    
-}
+int grid [M][N] = {{1,0,1,1},
+                  {0,1,1,0},
+                  {0,0,1,1},
+                  {1,0,0,0}};
 
-int get_biggest_region(vector< vector<int> > grid) {
-    int max=0;
-    for(int i=0;i<grid.size();i++){
-        for(int j=0;j<grid[0].size();j++){
-            int temp=calc(grid,i,j);
-            if(temp>max)
-                max=temp;
+int connected_grid(bool visited[M][N],int i, int j, int &count, int &max){
+  if(grid[i][j]==1){
+    count++;
+    visited[i][j]=true;
+    for(int l = i-1;l<=i+1;l++){
+      for(int k = j-1;k<=j+1;k++){
+        if(visited[l][k]==false && l>=0 && k>=0 && l<M && k<N){
+          connected_grid(visited,l,k,count,max);
         }
+      }
     }
-    return max;
+    if(max<count)
+      max = count;
+    count = 0;
+    visited[i][j]=false;
+  }
 }
 
 
 int main(){
-    int n;
-    cin >> n;
-    int m;
-    cin >> m;
-    vector< vector<int> > grid(n,vector<int>(m));
-    for(int grid_i = 0;grid_i < n;grid_i++){
-       for(int grid_j = 0;grid_j < m;grid_j++){
-          cin >> grid[grid_i][grid_j];
-       }
+  bool visited[M][N]={{false}};
+  int max = -1;
+  int count=0;
+  for(int i =0;i<M;i++){
+    for(int j = 0;j<N;j++){
+      connected_grid(visited, i, j,count,max);
     }
-    cout << get_biggest_region(grid) << endl;
-    return 0;
+  }
+  cout<<max;
 }
-
+  
